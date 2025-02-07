88fef503ed60a959e710663384683bba17746fdd
Diego MS <diegoms.0606@gmail.com>
2025-02-07 14:57:31 -0600

    agregando el alias

--- master.py
+++ master.py
@@ -1,7 +1,8 @@
 nombre = "Diego"
 apellido = "Miguel"
+alias = "DMS"
 
 # primer cambio: se optimizo el saludo
 saludo = f"Hola {nombre} {apellido}"
 print(saludo)
-print(f"Hola {nombre} {apellido} ¿como estas?")
+print(f"Hola {nombre} {apellido}({alias}) ¿como estas?")
